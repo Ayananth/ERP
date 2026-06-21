3a26de3a2b8a4c1e1550bf083ac245e12ed83632
Ayananth <tsayananth93@gmail.com>
2026-06-21 21:44:58 +0530

    admin site registration

--- backend/core/inventory/admin.py
+++ backend/core/inventory/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
-from .models import Item, Shelf, ItemGroup, Manufacturer, Unit
+from .models import Item, Shelf, ItemGroup, Manufacturer, Unit, ItemUnit, ItemPrice, ItemPhoto
 # Register your models here.
 
 admin.site.register(Item)
 admin.site.register(Shelf)
 admin.site.register(ItemGroup)
 admin.site.register(Manufacturer)
+admin.site.register(ItemUnit)
+admin.site.register(ItemPrice)
+admin.site.register(ItemPhoto)
 @admin.register(Unit)
 class UnitAdmin(admin.ModelAdmin):
     list_display = ("code", "name")
