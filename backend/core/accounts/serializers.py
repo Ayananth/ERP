fe702a55d58239ea379a89541a94e33f720499b8
Ayananth <tsayananth93@gmail.com>
2026-06-22 00:29:10 +0530

    basic credential validations

--- backend/core/accounts/serializers.py
+++ backend/core/accounts/serializers.py
@@ -1,3 +1,4 @@
+import re
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
@@ -13,7 +14,32 @@ class RegisterSerializer(serializers.ModelSerializer):
             "password",
         ]
 
+    def validate_username(self, value):
+
+        value = value.strip()
+
+        if User.objects.filter(username=value).exists():
+            raise serializers.ValidationError(
+                "Username already exists."
+            )
+
+        if not re.match(r"^[A-Za-z][A-Za-z0-9_]*$", value):
+            raise serializers.ValidationError(
+                "Username must start with a letter and contain only letters, numbers, and underscores."
+            )
+
+        return value
+
+    def validate_password(self, value):
+
+        if len(value) < 6:
+            raise serializers.ValidationError(
+                "Password must be at least 6 characters long."
+            )
+        return value
+
     def create(self, validated_data):
+
         password = validated_data.pop("password")
 
         user = User(**validated_data)
