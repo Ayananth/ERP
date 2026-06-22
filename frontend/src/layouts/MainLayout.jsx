8fddd83ad6c8146b09a382114324bbf4a0cd87f8
Ayananth <tsayananth93@gmail.com>
2026-06-22 14:43:25 +0530

    header in main layout

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,14 +1,23 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "../components/layout/Sidebar";
+import Header from "../components/layout/Header";
 
 function MainLayout() {
   return (
     <div className="flex h-screen">
+      {/* Sidebar */}
       <Sidebar />
 
-      <main className="flex-1 overflow-auto">
-        <Outlet />
-      </main>
+      {/* Right Section */}
+      <div className="flex flex-col flex-1 overflow-hidden">
+        {/* Header */}
+        <Header />
+
+        {/* Page Content */}
+        <main className="flex-1 overflow-auto p-6 bg-slate-50">
+          <Outlet />
+        </main>
+      </div>
     </div>
   );
 }
