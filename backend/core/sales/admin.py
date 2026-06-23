ae884c8a1e1766c1dfa5cccb29ae8c25708282fc
Ayananth <tsayananth93@gmail.com>
2026-06-23 12:50:43 +0530

    sales models registered

--- backend/core/sales/admin.py
+++ backend/core/sales/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Customer, SalesQuotation, SalesQuotationLine, SalesOrder, SalesOrderLine
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(SalesQuotation)
+admin.site.register(SalesQuotationLine)
+admin.site.register(SalesOrder)
+admin.site.register(SalesOrderLine)
